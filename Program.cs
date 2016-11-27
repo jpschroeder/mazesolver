0dcabe909ce25651c653b6503f1330692aa685f1
John Schroeder <john@schroederspace.com>
2016-11-26 23:40:41 -0500

    working solver

--- Program.cs
+++ Program.cs
@@ -13,8 +13,11 @@ namespace ConsoleApplication
             int dimen = 20;
             Console.Clear();
             Console.CursorVisible = false;
-            var maze = MazeGenerator.GenerateMaze(dimen, dimen);
-            MazeGenerator.RenderMaze(maze);
+            while(true)
+            {
+                var maze = Maze.GenerateMaze(dimen, dimen);
+                var path = Maze.SolveMaze(maze);
+            }
         }
     }
 
@@ -26,9 +29,68 @@ namespace ConsoleApplication
         West = 8
     }
 
-    public class MazeGenerator
+    public class Maze
     {
         private static Random rand = new Random();
+
+        public static ImmutableList<Tuple<int,int>> SolveMaze(int[,] grid)
+        {
+            int startx = 0;
+            int starty = rand.Next(grid.GetUpperBound(0));
+            var start = new Tuple<int,int>(startx, starty);
+
+            int finishx = grid.GetUpperBound(1);
+            int finishy = rand.Next(grid.GetUpperBound(0));
+            var finish = new Tuple<int,int>(finishx, finishy);
+
+            var path = ImmutableList.Create<Tuple<int,int>>(start);
+            var visited = new HashSet<Tuple<int,int>>();
+            visited.Add(start);
+            return SolveMaze(grid, path, visited, finishx, finishy);
+        }
+
+        public static ImmutableList<Tuple<int,int>> SolveMaze(
+            int[,] grid,
+            ImmutableList<Tuple<int,int>> currentPath,
+            HashSet<Tuple<int,int>> visited,
+            int finishX, int finishY)
+        {
+            RenderMaze(grid, currentPath.Add(new Tuple<int,int>(finishX, finishY)));
+            int currentX = currentPath.Last().Item1;
+            int currentY = currentPath.Last().Item2;
+            var dir = (Direction[])Enum.GetValues(typeof(Direction));
+            var directions = dir.Where(d => DoorExists(grid[currentY,currentX], d)).OrderBy(x => rand.Next());
+
+            foreach(var direction in directions)
+            {
+                int nextX = currentX + OffsetX(direction);
+                int nextY = currentY + OffsetY(direction);
+                var next = new Tuple<int,int>(nextX, nextY);
+
+                if (!ValidX(nextX, grid) || !ValidY(nextY, grid))
+                {
+                    continue;
+                }
+
+                if (nextX == finishX && nextY == finishY)
+                {
+                    return currentPath.Add(next);
+                }
+
+                if (visited.Contains(next))
+                {
+                    continue;
+                }
+                visited.Add(next);
+
+                var ret = SolveMaze(grid, currentPath.Add(next), visited, finishX, finishY);
+                if (ret != null)
+                {
+                    return ret;
+                }
+            }
+            return null;
+        }
         public static int[,] GenerateMaze(int height, int width)
         {
             var grid = new int[height,width];
@@ -63,9 +125,17 @@ namespace ConsoleApplication
                 CarvePassages(grid, nextX, nextY);
             }
         }
-
-        public static void RenderMaze(int[,] grid, int currentX = -1, int currentY = -1)
+        public static void RenderMaze(int[,] grid)
         {
+            RenderMaze(grid, null);
+        }
+        public static void RenderMaze(int[,] grid, int x, int y)
+        {
+            RenderMaze(grid, ImmutableList.Create<Tuple<int,int>>(new Tuple<int,int>(x,y)));
+        }
+        public static void RenderMaze(int[,] grid, ImmutableList<Tuple<int,int>> highlightList)
+        {
+            var highlight = highlightList == null? null : highlightList.ToImmutableHashSet();
             int height = grid.GetUpperBound(0) + 1;
             int width = grid.GetUpperBound(1) + 1;
             Console.SetCursorPosition(0,0);
@@ -81,10 +151,11 @@ namespace ConsoleApplication
                 Console.Write("|");
                 for(int x = 0; x < width; x++)
                 {
-                    var mid = DirectionExists(grid[y,x], Direction.South)? "_" : " ";
-                    if (currentX != -1 && currentY != -1 && x == currentX && y == currentY)
+                    var mid = WallExists(grid[y,x], Direction.South)? "_" : " ";
+                    bool hl = (highlight != null && highlight.Contains(new Tuple<int,int>(x,y)));
+                    if (hl)
                     {
-                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.BackgroundColor = ConsoleColor.Blue;
                         Console.Write(mid);
                         Console.ResetColor();
                     }
@@ -93,22 +164,36 @@ namespace ConsoleApplication
                         Console.Write(mid);
                     }
 
-                    if (DirectionExists(grid[y,x], Direction.East))
+                    if (WallExists(grid[y,x], Direction.East))
                     {
                         Console.Write("|");
                     }
                     else
                     {
-                        Console.Write(DirectionExists(grid[y,x], Direction.South)? "_" : " ");
+                        var s = WallExists(grid[y,x], Direction.South)? "_" : " ";
+                        if (hl)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.Write(s);
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write(s);
+                        }
                     }
                 }
                 Console.Write("\n");
             }
         }
 
-        public static bool DirectionExists(int gridelem, Direction direction)
+        public static bool WallExists(int gridelem, Direction direction)
         {
-            return !((gridelem & (int)direction) != 0);
+            return !DoorExists(gridelem, direction);
+        }
+        public static bool DoorExists(int gridelem, Direction direction)
+        {
+            return ((gridelem & (int)direction) != 0);
         }
 
         public static bool ValidX(int val, int[,] grid)
